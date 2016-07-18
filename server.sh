eef0d70bbec25eac152f6145aee5dcf94063f589
Wouter De Borger <wouter.deborger@inmanta.com>
2016-07-18 10:56:28 +0200

    added comments

--- server.sh
+++ server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#setup repos
 cat > /etc/yum.repos.d/inmanta.repo <<EOF
 [inmanta-master]
 baseurl=https://packages.inmanta.com/rpms/inmanta/master/fedora/
@@ -17,12 +18,17 @@ enabled=1
 gpgcheck=0
 EOF
 
+#install all packages at once
 dnf install -y python3-inmanta-server inmanta-dashboard python3-greenlet python3-pymongo-gridfs mongodb-server python3-blessings
 
-echo "smallfiles = true" >>/etc/mongod.conf 
+#optimize mongo for small db and fast start
+echo "smallfiles = true" >>/etc/mongod.conf
+
+#start mongo 
 systemctl start mongod
 systemctl enable mongod
 
+#setup the server
 cat > /etc/inmanta/server.cfg <<EOF
 [config]
 # The directory where the server stores its state
@@ -51,12 +57,16 @@ EOF
 systemctl start inmanta-server
 systemctl enable inmanta-server
 
+#patch up hosts, for agent autostart
 echo "192.168.33.101 vm1" >> /etc/hosts
 echo "192.168.33.102 vm2" >> /etc/hosts
 
-
+#set keys for agent access
 cp /vagrant/vagrant-master /root/.ssh/id_rsa
+mkdir /var/lib/inmanta/.ssh
 cp /vagrant/vagrant-master /var/lib/inmanta/.ssh/id_rsa
-chown inmanta /var/lib/inmanta/.ssh/id_rsa
-sudo -u inmanta sh -c "ssh-keyscan -H vm1 >> ~/.ssh/known_hosts"
+chown inmanta -R /var/lib/inmanta/.ssh
 
+#add host to known hosts to prevent warningsz
+sudo -u inmanta sh -c "ssh-keyscan -H vm1 >> ~/.ssh/known_hosts"
+sudo -u inmanta sh -c "ssh-keyscan -H vm2 >> ~/.ssh/known_hosts"
